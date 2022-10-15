2ec89e883c427bdea0432a1836b5fae83c7e1389
orhanbenli <orhanbenli7@outlook.com>
2022-10-15 14:19:37 -0400

    Update some files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const { response } = require("express");
 
 const db = knex({
   client: "pg",
@@ -59,13 +60,16 @@ app.post("/signin", (req, res) => {
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
   db("users")
+    .returning("*")
     .insert({
       email: email,
       name: name,
       joined: new Date(),
     })
-    .then(console.log);
-  res.json(database.users[database.users.length - 1]);
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => res.status(400).json("Unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
