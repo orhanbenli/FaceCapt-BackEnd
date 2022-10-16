403b229d8b526175d2146fd68554de58b55b6258
orhanbenli <orhanbenli7@outlook.com>
2022-10-16 01:10:46 -0400

    Update some files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-concentric-19972",
     port: 5432,
     user: "orhanbenli",
     password: "",
